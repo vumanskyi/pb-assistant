39838545598315480f172d76377f78cc4fee840c
Vlad Umanskyi <vladumanskyi@gmail.com>
2019-10-14 21:36:23 +0300

    Some fixes

--- src/Controller/Api/ApiController.php
+++ src/Controller/Api/ApiController.php
@@ -2,17 +2,39 @@
 namespace App\Controller\Api;
 
 use FOS\RestBundle\Controller\AbstractFOSRestController;
-use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Validator\ConstraintViolationList;
 
+/**
+ * @author Vlad Umanskyi
+ */
 class ApiController extends AbstractFOSRestController
 {
     /**
-     * @Route("/api", name="app_api_home")
+     * @param ConstraintViolationList $validator
+     * @return array
      */
-    public function index()
+    protected static function getErrors(ConstraintViolationList $validator): array
+    {
+        $errorList = [];
+        foreach ($validator as $error) {
+            $errorList[$error->getPropertyPath()] = $error->getMessage();
+        }
+        return $errorList;
+    }
+
+    /**
+     * @param ConstraintViolationList $validator
+     * @return \Symfony\Component\HttpFoundation\JsonResponse
+     */
+    public function getErrorResponse(ConstraintViolationList $validator)
     {
         return $this->json(
-            ['test' => __METHOD__]
+            [
+                'title' => 'Errors',
+                'errors' => static::getErrors($validator)
+            ],
+            Response::HTTP_BAD_REQUEST
         );
     }
 }
--- src/Controller/Api/VocabularyController.php
+++ src/Controller/Api/VocabularyController.php
@@ -2,19 +2,17 @@
 namespace App\Controller\Api;
 
 use App\Entity\Vocabulary;
-use App\Form\VocabularyFormType;
 use App\Repository\VocabularyRepository;
-use FOS\RestBundle\Controller\AbstractFOSRestController;
 use FOS\RestBundle\Controller\Annotations as Rest;
-use Symfony\Component\Form\FormInterface;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Validator\Validator\ValidatorInterface;
 
 /**
  * @author Vlad Umanskyi
  * @Rest\Route("/api")
  */
-class VocabularyController extends AbstractFOSRestController
+class VocabularyController extends ApiController
 {
     /**
      * @var VocabularyRepository
@@ -52,14 +50,13 @@ class VocabularyController extends AbstractFOSRestController
 
     /**
      * @Rest\Get("/words/{id}", name="app_api_words_word")
-     * @param VocabularyRepository $repository
      * @param int $id
      * @return \FOS\RestBundle\View\View
      */
-    public function getWord(VocabularyRepository $repository, int $id)
+    public function getWord(int $id)
     {
         return $this->view(
-            $repository->findBy(['id' => $id]),
+            $this->getVocabularyRepository()->findBy(['id' => $id]),
             Response::HTTP_OK
         );
     }
@@ -67,23 +64,23 @@ class VocabularyController extends AbstractFOSRestController
     /**
      * @Rest\Post("/words", name="app_api_words_store")
      * @param Request $request
+     * @param ValidatorInterface $validator
      * @return \Symfony\Component\HttpFoundation\JsonResponse
      */
-    public function store(Request $request)
+    public function store(Request $request, ValidatorInterface $validator)
     {
         $vocabulary = new Vocabulary();
 
-        $form = $this->createForm(VocabularyFormType::class, $vocabulary);
+        $vocabulary->setTranslate($request->get('translate'));
+        $vocabulary->setWord($request->get('word'));
+        $vocabulary->setTranscription($request->get('transcription'));
+        $vocabulary->setExample($request->get('example'));
 
-        $form->submit($request->request->all());
+        $errors = $validator->validate($vocabulary);
 
-        $form->handleRequest($request);
+        if (count($errors) > 0) {
+            return $this->getErrorResponse($errors);
 
-        if ($form->isSubmitted() && !$form->isValid()) {
-            return $this->json([
-                'title' => 'There was a validation error',
-                'errors' => $this->getErrorsFromForm($form)
-            ]);
         }
         $em = $this->getDoctrine()->getManager();
         $em->persist($vocabulary);
@@ -95,25 +92,6 @@ class VocabularyController extends AbstractFOSRestController
         ], Response::HTTP_CREATED);
     }
 
-    /**
-     * @param FormInterface $form
-     * @return array
-     */
-    private function getErrorsFromForm(FormInterface $form)
-    {
-        $errors = array();
-        foreach ($form->getErrors(true, false) as $key => $error) {
-            $errors[] = $error->getMessage();
-        }
-        foreach ($form->all() as $childForm) {
-            if ($childForm instanceof FormInterface) {
-                if ($childErrors = $this->getErrorsFromForm($childForm)) {
-                    $errors[$childForm->getName()] = $childErrors;
-                }
-            }
-        }
-        return $errors;
-    }
 //
 //    /**
 //     * @Rest\Put("/words/{id}, name="app_api_words_update")
